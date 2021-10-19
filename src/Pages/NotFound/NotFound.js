96623c086142ae13b644a9116c569974b6d1e124
sumaiaakter <sumaiamannan2013@gmail.com>
2021-10-19 09:25:19 -0500

    redirect

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -4,7 +4,7 @@ import notfound from '../../images/404.jpg'
 const NotFound = () => {
     return (
         <div>
-            <img width="100%" src={notfound} alt="" />
+            <img width="50%" src={notfound} alt="" />
             <br />
             <Link to="/"><button className="btn btn-primary">Go Back</button></Link>
         </div>
